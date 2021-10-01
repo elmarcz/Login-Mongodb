b10fd984f21c15d15727f18edd48a556efb19f55
Marc Medrano <83883889+elmarcz@users.noreply.github.com>
2021-10-01 18:14:06 +0200

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/users/singin', (req, res) => {
+    res.send('Sing In')
+})
+
+router.get('/users/singup', (req, res) => {
+    res.send('Autentication Formulary')
+})
+
 module.exports = router;
