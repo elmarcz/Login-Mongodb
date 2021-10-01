5bcf3126019fbae42a7076526a6df13bca6f4592
Marc Medrano <83883889+elmarcz@users.noreply.github.com>
2021-10-01 18:14:33 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,1 +1,5 @@
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/notes-db-app')
+    .then(db => console.log('Database is conected'))
+    .catch(err => console.error(err))
