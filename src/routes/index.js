aeffab1b1da678c45a79081a58f7a1db276edc1f
Marc Medrano <83883889+elmarcz@users.noreply.github.com>
2021-10-01 19:57:11 +0200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Index')
+    res.render('index')
 })
 
 router.get('/about', (req, res) => {
-    res.send('About')
+    res.render('about')
 })
 module.exports = router;
